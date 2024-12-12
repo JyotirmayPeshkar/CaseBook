d878acc5bb0e6f0adce8a23284267be72c26bcfa
JyotirmayPeshkar <jrpeshkar@gmail.com>
2024-12-12 08:43:21 +0530

    complete routing and reposive toolbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { ServicesComponent } from './views/services/services.component';
+import { HomeComponent } from './views/home/home.component';
+import { ResumeComponent } from './views/resume/resume.component';
+import { PortfolioComponent } from './views/portfolio/portfolio.component';
+import { ContactComponent } from './views/contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'Home', pathMatch: 'full' }, // Default route
+  { path: 'Home', component: HomeComponent },
+  { path: 'Services', component: ServicesComponent },
+  { path: 'Resume', component: ResumeComponent },
+  { path: 'Portfolio', component: PortfolioComponent },
+  { path: 'Contact', component: ContactComponent },
+];
+
---
+++ src/app/views/toolbar/toolbar.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import { RouterLink } from '@angular/router';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatIconModule} from '@angular/material/icon';
+import { CommonModule } from '@angular/common';
+@Component({
+  selector: 'app-toolbar',
+  imports: [MatToolbarModule,MatButtonModule,RouterLink,MatMenuModule,MatIconModule,CommonModule],
+  templateUrl: './toolbar.component.html',
+  styleUrl: './toolbar.component.scss'
+})
+export class ToolbarComponent {
+  isSmallScreen = false;
+
+  constructor(private breakpointObserver: BreakpointObserver) {
+    this.breakpointObserver.observe([Breakpoints.Handset, Breakpoints.Tablet])
+      .subscribe(result => {
+        this.isSmallScreen = result.matches;
+      });
+  }
+}
