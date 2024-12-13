7a3bd070301f321b9d6a89a223db1116e72d7f53
JyotirmayPeshkar <jrpeshkar@gmail.com>
2024-12-13 18:25:19 +0530

    home and toolbar complete

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import 'boxicons'
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class HomeComponent {
-
+  words = ['Coder', 'Youtuber', 'Blogger', 'Designer']; // Array of words
 }
--- src/app/views/toolbar/toolbar.component.ts
+++ src/app/views/toolbar/toolbar.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatButtonModule} from '@angular/material/button';
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import {MatMenuModule} from '@angular/material/menu';
 import {MatIconModule} from '@angular/material/icon';
 import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-toolbar',
-  imports: [MatToolbarModule,MatButtonModule,RouterLink,MatMenuModule,MatIconModule,CommonModule],
+  imports: [MatToolbarModule,MatButtonModule,RouterLink,MatMenuModule,MatIconModule,CommonModule,RouterLinkActive],
   templateUrl: './toolbar.component.html',
   styleUrl: './toolbar.component.scss'
 })
