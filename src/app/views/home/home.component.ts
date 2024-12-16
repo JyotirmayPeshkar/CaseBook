69cca375d38fb35ca16d989e90c77151cd306d43
JyotirmayPeshkar <jrpeshkar@gmail.com>
2024-12-16 19:10:04 +0530

    responsive

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -11,4 +11,21 @@ import 'boxicons'
 })
 export class HomeComponent {
   words = ['Coder', 'Youtuber', 'Blogger', 'Designer']; // Array of words
+  dynamicContentArray!: string[];
+
+
+  constructor() {
+    // Initialize the array with content (can include HTML)
+    this.dynamicContentArray = [
+      ` With a strong foundation in backend development and a knack for bringing ideas to life,
+       I focus on crafting efficient and scalable systems that power the web. Whether it's tackling
+        complex logic or optimizing performance, I thrive on challenges that allow me to learn and grow. 
+        Let’s connect and build something amazing together! `
+    ];
+  }
+
+  // Method to dynamically change array contents later
+  updateContent(newContent: string, index: number): void {
+    this.dynamicContentArray[index] = newContent;
+  }
 }
